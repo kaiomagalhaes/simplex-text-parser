7a93357b82b38b97db2d1a310febde96d26669c6
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-05 12:43:11 -0300

    creating ruby gem

---
+++ lib/simplex/text/parser.rb
@@ -0,0 +1,9 @@
+require "simplex/text/parser/version"
+
+module Simplex
+  module Text
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simplex/text/parser/version.rb
@@ -0,0 +1,7 @@
+module Simplex
+  module Text
+    module Parser
+      VERSION = "0.1.0"
+    end
+  end
+end
