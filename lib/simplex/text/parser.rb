b4e4a8a6a22f1d176d5ba9af8a12c0cfea5c5954
Kaio Cristian Costa Porto De Magalhães <contato@kaiomagalhaes.com.br>
2015-05-06 00:42:55 +0000

    setting matrix_parser to fail

--- lib/simplex/text/parser.rb
+++ lib/simplex/text/parser.rb
@@ -1,27 +1,21 @@
 require "simplex/text/parser/version"
 require "simplex/text/parser/coefficients_parser"
 require "simplex/text/parser/rhs_parser"
+require "simplex/text/parser/matrix_parser"
 
 module Simplex
 	module Text
 		module Parser
 			include CoefficientsParser
 			include RhsParser
+			include MatrixParser
 
 			def parse(str)
 				{
 					coefficients: extract_coefficients(str),
-					matrix:findMatrix,
-					rhs:findRhs(str)
+					matrix: extract_matrix(str),
+					rhs: extract_rhs(str)
 				}
-			end
-
-			def findMatrix
-				[[2,1],[1,2]]
-			end
-
-			def findRhs(str)
-				extract_rhs(str)
 			end
 
 		end
---
+++ lib/simplex/text/parser/matrix_parser.rb
@@ -0,0 +1,16 @@
+module Simplex
+	module Text
+		module Parser
+			module MatrixParser
+
+				def extract_matrix(str)
+					nil
+				end
+
+				def find_matrix(str)
+					str.lines.first
+				end
+			end
+		end
+	end
+end
